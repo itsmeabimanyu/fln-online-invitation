f630107093e1b9bd4ee5c79f46d5545263cbbd87
abimanyugithub <abimanyubima@hotmail.com>
2025-02-14 07:59:18 +0700

    fixed bug login after logout

--- applications/invitation/context_processors.py
+++ applications/invitation/context_processors.py
@@ -1,12 +1,16 @@
 def footer_copyright(request):
     user = request.user  # Mengambil user dari request
 
-    # Membuat string username dengan format yang diinginkan
-    if user.first_name or user.last_name:
-        username = f"{user.first_name.capitalize()} {user.last_name.capitalize()}".strip()
+    # Pastikan user sudah terautentikasi
+    if user.is_authenticated:
+        # Membuat string username dengan format yang diinginkan
+        if user.first_name or user.last_name:
+            username = f"{user.first_name.capitalize()} {user.last_name.capitalize()}".strip()
+        else:
+            username = user.username.capitalize()
     else:
-        username = user.username.capitalize()
-
+        username = 'Guest'  # Atau nilai default lain jika user tidak terautentikasi
+        
     return {
         'footer_copyright': '© 2025 - IT Frina Lestari Nusantara, Inc. All rights reserved.',
         'footer': '<li class="list-inline-item"><a class="text-muted" href="https://github.com/itsmeabimanyu/fln-online-invitation" target="_blank"><i class="align-middle me-2" data-feather="github"></i></a></li>',
